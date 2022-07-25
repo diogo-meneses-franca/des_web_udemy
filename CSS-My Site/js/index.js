783d77da33dbf7504f5d422c4cb98b31e8e50cfb
Diogo Meneses França <diogo.meneses.franca@gmail.com>
2022-07-25 12:36:29 -0300

    praticando JavaScript

---
+++ CSS-My Site/js/index.js
@@ -0,0 +1,4 @@
+var dogAge = prompt("How old is your dog?");
+var dogAge = parseInt(dogAge);
+var humanAge = (dogAge -2) * 4 + 21;
+console.log("Your dog is " + humanAge + " years old in human years.");
